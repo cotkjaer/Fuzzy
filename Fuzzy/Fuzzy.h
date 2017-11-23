7a2cdb11258ed6c041df55d5a9509c527a651b83
Christian Otkjær <christianotkjaer@icloud.com>
2017-11-23 21:54:05 +0100

    Initial Commit

---
+++ Fuzzy/Fuzzy.h
@@ -0,0 +1,19 @@
+//
+//  Fuzzy.h
+//  Fuzzy
+//
+//  Created by Christian Otkjær on 23/11/2017.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fuzzy.
+FOUNDATION_EXPORT double FuzzyVersionNumber;
+
+//! Project version string for Fuzzy.
+FOUNDATION_EXPORT const unsigned char FuzzyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fuzzy/PublicHeader.h>
+
+
